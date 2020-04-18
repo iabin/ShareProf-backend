4e1a0ad63a5aa83b44f33c081de2adbd8945b3ea
Iabin <iabinelo@gmail.com>
2020-04-17 22:04:12 -0500

    Modelos agregados

--- shareProf/urls.py
+++ shareProf/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from shareProf.api.views import auth_views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', auth_views.UserViewSet)
+router.register(r'groups', auth_views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
