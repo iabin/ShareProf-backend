897cf668be7981cbb31bb7f71c8b9faa745846a4
Iabin <iabinelo@gmail.com>
2020-04-17 22:22:53 -0500

    auth agregado

--- shareProf/urls.py
+++ shareProf/urls.py
@@ -25,4 +25,6 @@ router.register(r'groups', auth_views.GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),#Agrega el boton de logout y login
+    path('api/', include('shareProf.api.urls'))
 ]
